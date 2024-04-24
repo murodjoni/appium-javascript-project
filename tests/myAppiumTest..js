1a6dae1105ea19beaee1ca44e83523bd1de3187c
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-04-23 21:23:55 -0600

    Continuing Test framework - Added Appium options

---
+++ tests/myAppiumTest..js
@@ -0,0 +1,13 @@
+const wd = require("wd");
+const chai = require("chai");
+const { assert } = chai;
+
+// Set up Appium options
+const serverConfig = {
+    host: "localhost",
+    port: 4723, // Default Appium port
+};
+
+describe("My Appium Test Suite", function () {
+    before(async function () {});
+});
